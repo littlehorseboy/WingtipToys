2187e27fb38417d02cc7beb540e7b25174b482f1
littlehorseboy <littlehorseboy@gmail.com>
2022-05-08 00:09:23 +0800

    feat(): checkout

---
+++ Models/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public decimal Total { get; set; }
+
+        public string PaymentTransactionId { get; set; }
+
+        public bool HasBeenShipped { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
